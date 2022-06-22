8c16f8328157ee44f22094821d5f63248e17b22f
rxdjina <55474317+rxdjina@users.noreply.github.com>
2022-06-22 13:16:08 -0700

    initial project

---
+++ twitter/View Controllers/TimelineViewController.m
@@ -0,0 +1,56 @@
+//
+//  TimelineViewController.m
+//  twitter
+//
+//  Created by emersonmalca on 5/28/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import "TimelineViewController.h"
+#import "APIManager.h"
+
+@interface TimelineViewController ()
+
+@end
+
+@implementation TimelineViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Get timeline
+    [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
+        
+        NSLog(@"%@", tweets);
+        NSLog(@"%lu@", tweets.count);
+//        NSLog(<#NSString * _Nonnull format, ...#>)
+        
+        if (tweets) {
+            NSLog(@"Successfully loaded home timeline");
+            for (NSDictionary *dictionary in tweets) {
+                NSString *text = dictionary[@"text"];
+                NSLog(@"%@", text);
+            }
+        } else {
+            NSLog(@"Error getting home timeline: %@", error.localizedDescription);
+        }
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+@end
